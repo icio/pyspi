90ab7ecb82933501f2fc9c072d9fac7f5587f83a
Paul Scott <paul.scotto@gmail.com>
2015-09-22 14:28:08 +0100

    Add github3.py dependency

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     version=__version__,
     packages=['pyspi'],
     author='Paul Scott <paul.scotto@gmail.com>',
+    install_requires=['github3.py >0.9.0,<1.0.0'],
     entry_points={
         'console_scripts': [
             'pyspi = pyspi.__main__:main'
